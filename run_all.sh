f182b832e2eaa3362272b49640ce3204fff3606f
Yamac <yamac619@gmail.com>
2021-10-09 19:38:34 -0400

    Learning 2-d with sigmoid

--- run_all.sh
+++ run_all.sh
@@ -2,13 +2,13 @@
 
 
 
-python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 5  -timetovec 0 -softmax 1
-#python main.py -data power_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 0 -softmax 1
-#python main.py -data sin_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 0 -softmax 1
-##
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 20  -timetovec 1 -softmax 1
-#python main.py -data power_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 1 -softmax 1
-#python main.py -data sin_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 1 -softmax 1
+python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 20  -timetovec 0 -softmax 1
+python main.py -data power_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 0 -softmax 1
+python main.py -data sin_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 0 -softmax 1
+#
+python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 20  -timetovec 1 -softmax 1
+python main.py -data power_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 1 -softmax 1
+python main.py -data sin_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr .0001 -batch 20  -timetovec 1 -softmax 1
 #
 #
 #python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 100 -lr 0.0001 -batch 20  -timetovec 1 -softmax 1
