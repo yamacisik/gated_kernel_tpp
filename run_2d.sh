4f4fd300b03ae432292e0617684d50a6a088269a
Yamac <yamac619@gmail.com>
2021-10-11 00:56:47 -0400

    Learning 2-d with sigmoid

--- run_2d.sh
+++ run_2d.sh
@@ -9,8 +9,7 @@
 
 
 
-python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 1 -softmax 1 -regularize 0
-python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 1 -regularize 0
+
 python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 1 -softmax 1 -regularize 0
 python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 1 -regularize 0
 
