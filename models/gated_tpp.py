18e227320853f2d9763a396f9135e2d9e702852e
Yamac <yamac619@gmail.com>
2021-10-12 02:31:01 -0400

    Learning 2-d with sigmoid

--- models/gated_tpp.py
+++ models/gated_tpp.py
@@ -25,7 +25,7 @@ class gated_tpp(nn.Module):
                                kernel_type=kernel_type, alpha=alpha, test_softmax=softmax, embed_time=embed_time,
                                timetovec=timetovec, s=s, l=l, p=p,sigma= sigma,regulizing_param=regulizing_param,betas = betas)
         self.norm = nn.LayerNorm(d_model, eps=1e-6)
-        self.decoder = Decoder(num_types, d_model, dropout)
+        self.decoder = Decoder(num_types, d_model+d_type, dropout)
 
     def forward(self, event_type, event_time, arrival_times):
         scores, embeddings, _ = self.encoder(event_type, event_time, arrival_times)
@@ -190,7 +190,7 @@ class Encoder(nn.Module):
         if self.num_types ==1:
             hidden_vector = temp_enc
         else:
-            hidden_vector  = temp_enc +type_embedding
+            hidden_vector = torch.cat([temp_enc, type_embedding])
 
         ## Future Masking
         subsequent_mask = get_subsequent_mask(event_type)
