692468c6d7d18dc5d2f9b078cd12870b679d8487
Yamac <yamac619@gmail.com>
2021-10-13 18:37:07 -0400

    Learning 2-d with sigmoid

--- run_2d.sh
+++ run_2d.sh
@@ -22,11 +22,11 @@
 #python main.py -data 2_d_hawkes -d_model 20 -d_type 20 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
 #python main.py -data 2_d_hawkes -d_model 24 -d_type 24 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
 
-python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 1.0 -beta_2 1.0
-python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 1.0 -beta_2 1.0
+python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0
+python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0
 
-python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 1.0 -beta_2 1.0
-python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 1.0 -beta_2 1.0
+python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0
+python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0
 #python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.7 -beta_2 0.4
 
 #python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.7 -beta_2 0.5
--- util/kernel_functions.py
+++ util/kernel_functions.py
@@ -548,7 +548,7 @@ class magic_kernel_2(nn.Module):
 
         else:
             self.lengthscale = nn.Sequential(nn.Linear(d_type * 2, 1, bias=False), nn.Softplus(self.betas[0]))
-            self.sigma = nn.Sequential(nn.Linear(d_type * 2, 1, bias=False), nn.Sigmoid())
+            self.sigma = nn.Sequential(nn.Linear(d_type * 2, 1, bias=False), nn.Softplus(self.betas[2]))
             self.alpha = nn.Sequential(nn.Linear(d_type * 2, 1, bias=False), nn.Softplus(self.betas[1]))
             # self.s = nn.Sequential(nn.Linear(d_type * 2, 1, bias=False), nn.Sigmoid())
             # self.base_intensity = nn.Sequential(nn.Linear(d_type, 1, bias=False), nn.Softplus(self.betas[2]))
@@ -580,7 +580,7 @@ class magic_kernel_2(nn.Module):
         else:
             if not non_event_intensity:
                 lengthscale = self.lengthscale(combined_embeddings).squeeze(-1)
-                sigma = self.sigma(combined_embeddings).squeeze(-1)+5
+                sigma = self.sigma(combined_embeddings).squeeze(-1)
                 alpha = self.alpha(combined_embeddings).squeeze(-1)
                 # l =  self.l(combined_embeddings).squeeze(-1)
                 # s =  self.s(combined_embeddings).squeeze(-1)
