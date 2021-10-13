b8d41feac6bac82d924805307a448b0dacb9466d
Yamac <yamac619@gmail.com>
2021-10-13 04:48:45 -0400

    Learning 2-d with sigmoid

--- run_2d.sh
+++ run_2d.sh
@@ -22,8 +22,8 @@
 #python main.py -data 2_d_hawkes -d_model 20 -d_type 20 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
 #python main.py -data 2_d_hawkes -d_model 24 -d_type 24 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
 
-python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
-python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
+python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
+#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
 #python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
 #python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
 
