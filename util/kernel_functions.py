08741c555d87e3b06deb8d562210329acfc18381
Yamac <yamac619@gmail.com>
2021-10-13 04:24:24 -0400

    Learning 2-d with sigmoid

--- util/kernel_functions.py
+++ util/kernel_functions.py
@@ -591,7 +591,7 @@ class magic_kernel_2(nn.Module):
                 # alpha = self.parameter_layer(combined_embeddings)[:, :,:, 2].squeeze(-1)*6
 
                 self.param_loss= 0
-                self.param_loss += torch.abs(self.lengthscale[0](combined_embeddings)).mean()*5
+                self.param_loss += torch.abs(self.lengthscale[0](combined_embeddings)).mean()*6
                 self.param_loss += torch.abs(self.l[0](combined_embeddings)).mean() * 5
                 self.param_loss += torch.abs(self.sigma[0](combined_embeddings)).mean()*3
                 # self.param_loss = torch.abs(self.alpha[0](combined_embeddings)).mean()*0.5
@@ -609,7 +609,7 @@ class magic_kernel_2(nn.Module):
 
                 # base_intensity = self.base_intensity(combined_embeddings[:, :, :, self.d_type:]).squeeze(-1)
 
-        k1 = 1 + torch.tanh((d - l + 1) / s)
+        k1 = 1 + torch.tanh((d - l +2) / s)
         alpha = 1
         k2 =  (1 + (d ** 2) / (alpha * lengthscale ** 2)) ** (-alpha)
         # k2 = torch.exp(-(d) / lengthscale)
