97379b705a04479dc3f1ee48cdd78f9d2e62edc3
Yamac <yamac619@gmail.com>
2022-01-25 00:14:41 -0500

    Learning 2-d with sigmoid

--- run_2d.sh
+++ run_2d.sh
@@ -1,68 +1,30 @@
 #!/bin/bash
 
+#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.005 -batch 30  -timetovec 0 -softmax 0
+#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.005  -batch 30  -timetovec 0 -softmax 0
+#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.005  -batch 30  -timetovec 0 -softmax 0
+#
+#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
+#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
+#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
 
 
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0005 -batch 30  -timetovec 0 -softmax 1 -regularize 0
+for beta_1 in  0.6 0.4 0.2
+do
+for beta_2 in  0.6 0.4 0.2
+do
+for beta_3 in 0.6 0.4 0.2
+do
+python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 40 -lr 0.005  -batch 30  -timetovec 0 -softmax 0
+python main.py -data 2_d_hawkes -d_model 24 -d_type 24 -kernel_type 2  -epoch 40 -lr 0.005  -batch 30  -timetovec 0 -softmax 0
+python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 40 -lr 0.005 -batch 30  -timetovec 0 -softmax 0
 
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0005 -batch 30  -timetovec 0 -softmax 1 -regularize 0
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0005 -batch 30  -timetovec 0 -softmax 1 -regularize 0
-
-#python main.py -data 2_d_hawkes -d_model 8 -d_type 8 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 1 -softmax 0 -regularize 0
-#python main.py -data 2_d_hawkes -d_model 8 -d_type 8 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-
-
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 250 -lr 0.001 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-#python main.py -data 2_d_hawkes -d_model 8 -d_type 8 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-
-#python main.py -data 2_d_hawkes -d_model 10 -d_type 10 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
-#python main.py -data 2_d_hawkes -d_model 20 -d_type 20 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
-#python main.py -data 2_d_hawkes -d_model 24 -d_type 24 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
-
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0 -beta_3 0.7
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0 -beta_3 0.7
-#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0 -beta_3 0.7
-#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0 -beta_3 0.8
-#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0 -beta_3 0.6
-#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0 -beta_3 0.5
+python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 40 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
+python main.py -data 2_d_hawkes -d_model 24 -d_type 24 -kernel_type 2  -epoch 40 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
+python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 40 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
 
 
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.6 -beta_2 1.0 -beta_3 0.8
+done
+done
+done
 
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.7 -beta_2 0.4
-
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.7 -beta_2 0.5
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.7 -beta_2 0.6
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.7 -beta_2 0.4
-
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.4
-
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 25 -lr 0.0001 -batch 30  -timetovec 1 -softmax 0 -regularize 0 -beta_1 0.35 -beta_2 0.4
-
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 1 -softmax 0 -regularize 0 -beta_1 0.4 -beta_2 0.6
-
-#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.00005 -batch 30  -timetovec 0 -softmax 0 -regularize 0
-
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 2 -lr 0.001 -batch 30  -timetovec 0 -softmax 0
-
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.001  -batch 30  -timetovec 0 -softmax 0
-#python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.001  -batch 30  -timetovec 0 -softmax 0
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.001  -batch 30  -timetovec 0 -softmax 0
-
-python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.005 -batch 30  -timetovec 0 -softmax 0
-python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.005  -batch 30  -timetovec 0 -softmax 0
-python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.005  -batch 30  -timetovec 0 -softmax 0
-
-python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
-python main.py -data 2_d_hawkes -d_model 64 -d_type 64 -kernel_type 2  -epoch 50 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
-python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001  -batch 30  -timetovec 0 -softmax 0
-
-
-
-
-#python main.py -data 2_d_hawkes -d_model 16 -d_type 16 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 1 -softmax 1 -regularize 1
-#python main.py -data 2_d_hawkes -d_model 32 -d_type 32 -kernel_type 2  -epoch 50 -lr 0.0001 -batch 30  -timetovec 1 -softmax 1 -regularize 1
