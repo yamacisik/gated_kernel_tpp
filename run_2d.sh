5ba7369261da8c91dbcf6fa2c6c9b65cd327482d
Yamac <yamac619@gmail.com>
2022-01-26 23:45:39 -0500

    Preparing the code for ICML submission.

--- run_2d.sh
+++ run_2d.sh
@@ -5,7 +5,7 @@ data=2_d_hawkes
 
 python main.py -data mimic -d_model 16 -kernel_type 2  -epoch 250 -lr 0.001  -batch 32
 python main.py -data $data -d_model 16 -kernel_type 2  -epoch 100 -lr 0.001  -batch 32
-python main.py -data stackOverflow -d_model 16 -kernel_type 2  -epoch 100 -lr 0.001  -batch 32
-python main.py -data retweet -d_model 16 -kernel_type 2  -epoch 100 -lr 0.001  -batch 32
+python main.py -data stackoverflow -d_model 16 -kernel_type 2  -epoch 100 -lr 0.001  -batch 32
+python main.py -data retweet -d_model 16 -kernel_type 2  -epoch 100 -lr 0.0001  -batch 32
 
 
