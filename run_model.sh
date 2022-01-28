de29f0e736a9b96b91493302a1ba167b3523ed5b
Yamac <yamac619@gmail.com>
2022-01-28 01:16:29 -0500

    Preparing the code for ICML submission.

--- run_model.sh
+++ run_model.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-data=2_d_hawkes
+data=mimic
 
 
-python main.py -data $data -d_model 16 -epoch 250 -lr 0.001  -batch 32
+python main.py -data $data -d_model 16 -epoch 500 -lr 0.001  -batch 32
 
 
 
