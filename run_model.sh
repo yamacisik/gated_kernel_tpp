c09c54c039aad726241b6f039c3b377894eaa259
Yamac <yamac619@gmail.com>
2022-05-12 00:48:19 -0400

    Preparing the code for Neurips submission.

--- run_model.sh
+++ run_model.sh
@@ -4,6 +4,6 @@
 data=2_d_hawkes
 
 
-python main.py -data $data -d_model 16 -epoch 100 -lr 0.0001  -batch 16 -beta3 1
-python main.py -data $data -d_model 16 -epoch 100 -lr 0.0001  -batch 16 -beta3 5
-python main.py -data $data -d_model 16 -epoch 100 -lr 0.0001  -batch 16 -beta3 10
+python main.py -data $data -d_model 16 -epoch 100 -lr 0.0001  -batch 16 -b3 1
+python main.py -data $data -d_model 16 -epoch 100 -lr 0.0001  -batch 16 -b3 5
+python main.py -data $data -d_model 16 -epoch 100 -lr 0.0001  -batch 16 -b3 10
